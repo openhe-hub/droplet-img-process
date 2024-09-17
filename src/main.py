8d31179f67f329ff1aa8c75da6bb7cba7fba978c
openhe-hub <625529334@qq.com>
2024-09-17 22:17:24 +0800

    2024-09-17-upd: add substraction

--- src/main.py
+++ src/main.py
@@ -3,21 +3,29 @@ from process import *
 
 
 if __name__ == '__main__':
-    img = load_img('../assets/input/demo.jpg')
-    gray_img = gbr_to_gray(img)
-    denoised_img = denoise_img(gray_img)
+    id: int = 4
+    img = load_img(f'../assets/input/demo{id}.jpg')
+    background_img = load_img('../assets/input/background.jpg')
+    diff_img = diff_img(img, background_img)
+    gray_img = gbr_to_gray(diff_img)
+    binary_img = gbr_to_binary(gray_img)
+    denoised_img = denoise_img(binary_img)
     contours = find_contour_by_canny(denoised_img)
     contours = filter_contour(contours)
-    contour_img = cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR)
+    contour_img = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
+    contour_raw_img = img.copy()
     cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 2)
-    
+    cv2.drawContours(contour_raw_img, contours, -1, (0, 255, 0), 2)
+
     # display result
     cv2.imshow('origin', img)
     cv2.imshow('gray', gray_img)
+    cv2.imshow('binary', binary_img)
     cv2.imshow('denoised', denoised_img)
     cv2.imshow('contours', contour_img)
 
-    cv2.imwrite('../assets/output/contours.jpg', contour_img)
-    
+    cv2.imwrite(f'../assets/output/contours{id}.jpg', contour_img)
+    cv2.imwrite(f'../assets/output/contours{id}_on_raw.jpg', contour_raw_img)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
